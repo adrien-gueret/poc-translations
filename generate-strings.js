6e1902f1d0c0c9d0e7508e1c24b0f919e3fd0e55
adrien-gueret <adrien.gueret@openclassrooms.com>
2025-05-14 11:20:06 +0200

    chore(github): add workflow to build .strings files

---
+++ generate-strings.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+function jsonToStrings(jsonFilePath, outputFilePath) {
+  const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+  let stringsContent = '';
+
+  for (const [key, value] of Object.entries(jsonData)) {
+    stringsContent += `"${key}" = "${value}";
+`;
+  }
+
+  fs.writeFileSync(outputFilePath, stringsContent, 'utf8');
+  console.log(`Generated: ${outputFilePath}`);
+}
+
+const jsonFiles = ['en.json', 'fr.json'];
+
+jsonFiles.forEach((file) => {
+  const inputPath = path.join(__dirname, file);
+  const outputPath = path.join(__dirname, file.replace('.json', '.strings'));
+  jsonToStrings(inputPath, outputPath);
+});
